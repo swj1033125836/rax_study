74ddb495aa6770502cce5aa1fcb7ef3a9cbbe9ea
swj <1033125836@gmail.com>
2022-08-08 16:56:12 +0800

    Fragment无属性时缩写

--- src/components/study_component/HelloWords.tsx
+++ src/components/study_component/HelloWords.tsx
@@ -2,10 +2,10 @@ import { createElement, Fragment } from 'rax';
 
 function Welcomes() {
   return (
-    <Fragment>
+    <>
       <h1>Hello, </h1>
       <h1>World!!!!!</h1>
-    </Fragment>
+    </>
   );
 }
 export default Welcomes;
