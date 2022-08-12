88e2549472d3f3570029adec9f7827d76460f666
swj <1033125836@gmail.com>
2022-08-12 10:04:08 +0800

    log打印，chrom上可见 

--- src/components/study_component/HelloWord.tsx
+++ src/components/study_component/HelloWord.tsx
@@ -1,6 +1,11 @@
 import { createElement } from 'rax';
 
 function Welcome(props) {
+  console.log('================');
+  console.warn('456');
+  console.trace('789');
+  console.debug('111');
+  console.error('333');
   return <h1>Hello, {props.name}</h1>;
 }
 export default Welcome;
